ecfc22677b6941d15c01b5c5b03f3a201b0ac8ab
aspak309 <aspak0131@gmail.com>
2026-08-23 19:09:44 +0530

    Initialize Firebase app and authentication

---
+++ ​firebase.js
@@ -0,0 +1,46 @@
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+
+import {
+    getAuth,
+    GoogleAuthProvider,
+    RecaptchaVerifier,
+    signInWithPopup,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    sendPasswordResetEmail,
+    signInWithPhoneNumber,
+    signOut,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA76trG8L-GDKNuMKbtaORnuDfagRA3zY8",
+    authDomain: "gst-bill-maker-d7956.firebaseapp.com",
+    projectId: "gst-bill-maker-d7956",
+    storageBucket: "gst-bill-maker-d7956.firebasestorage.app",
+    messagingSenderId: "564339961180",
+    appId: "1:564339961180:web:0e9ff371695d0beeade599"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+
+export {
+    app,
+    auth,
+    GoogleAuthProvider,
+    RecaptchaVerifier,
+    signInWithPopup,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    sendPasswordResetEmail,
+    signInWithPhoneNumber,
+    signOut,
+    onAuthStateChanged
+};
